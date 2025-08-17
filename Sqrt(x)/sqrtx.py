cbe9e96a2b3c12eb242fd46c6d4f2d6f059becec
Arman Niaruhi <arman.niaruhi@gmail.com>
2025-08-17 09:22:53 +0200

    third solution

--- Sqrt(x)/sqrtx.py
+++ Sqrt(x)/sqrtx.py
@@ -16,3 +16,19 @@ def mySqrt(x):
 
         return res - 1
         '''
+        
+        
+        '''
+        Thirsd Solution
+        l = 0
+        r = x+1
+        
+        while(l < r):
+            m = (l + r) // 2
+            
+            if (m*m > x):
+                r = m
+            else:
+                l = m+1
+        return l-1
+        '''
