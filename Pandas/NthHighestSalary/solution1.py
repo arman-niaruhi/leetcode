f5245ff09cf8b323eea634980611b0ae2e504bd2
Arman Niaruhi <arman.niaruhi@gmail.com>
2025-08-18 10:01:11 +0200

    nth highes salary

--- Pandas/NthHighestSalary/solution1.py
+++ Pandas/NthHighestSalary/solution1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    # Rank the salaries
     salary_rank_set = sorted(set(employee['salary']), reverse = True)
     return pd.DataFrame([
         {
